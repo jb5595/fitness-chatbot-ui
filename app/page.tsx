54c0bce028e280c513826f5841d85e8d07de2c96
Jesse Berman <jesse.ben.berman@gmail.com>
2025-04-29 09:34:00 -0600

    moving app out of src and cleaning up page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 "use client"
-import PhoneNumberSelect from "./components/PhoneNumberSelect/PhoneNumberSelect"
+
 import ChatHistoryComponent from "./components/ChatHistoryComponent/ChatHistoryComponent";
-import { useState } from 'react'
 
 
 export default function Home() {
-  // const [selectedGym, setSelectedGym] = useState("");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <PhoneNumberSelect selectedGym={selectedGym} setSelectedGym={setSelectedGym}></PhoneNumberSelect> */}
-      {/* <ChatHistoryComponent></ChatHistoryComponent> */}
       <main className="flex flex-col gap-[32px] row-start-2 sm:items-start">
         <ChatHistoryComponent></ChatHistoryComponent>
       </main>

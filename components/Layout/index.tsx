ed345ab80ea9cd928c05a4294a09b631c2216f70
Jesse Berman <51378950+Jberma23@users.noreply.github.com>
2025-05-04 17:32:26 -0600

    Revert "fix pipeline"

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
 import Navbar from '../NavBar/Index'
  
-export type layoutProps = {
-    children: React.ReactNode;
-}
-export default function Layout({children}: layoutProps) {
+export default function Layout({ children }: any) {
   return (
     <>
       <Navbar />
